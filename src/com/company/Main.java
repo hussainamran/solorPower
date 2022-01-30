2c8049c8209cc6db0d346ef455487cb84fc8fc5a
hussainamran <hussainamran1978@gmail.com>
2022-01-30 16:05:31 -0500

    the basic statructure is complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,10 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
 
+        soloarsyatem solo=new soloarsyatem();
+        solo.moon=2;
+        solo.planets=1;
+        solo.sun=3;
+
     }
 }
